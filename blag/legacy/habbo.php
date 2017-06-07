60576b1ad1a02df20c105964515d529900e1d07d
Alex Birch <Birch-san@users.noreply.github.com>
2017-06-08 00:09:12 +0100

    HTTPD working

---
+++ blag/legacy/habbo.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Redirection to legacy/ succeeded</title>
+</head>
+<body>
+Redirection to legacy/ succeeded (unless you came here directly).
+</body>
+</html>
