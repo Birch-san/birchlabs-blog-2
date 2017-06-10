1b9286341c6a38204a99291d0613aaa447f68c0f
Alex Birch <Birch-san@users.noreply.github.com>
2017-06-10 18:17:35 +0100

    theoretically this should work. it's a shame that it doesn't.

--- blag/index.php
+++ blag/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Hey there!</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="preload" href="style.css" as="style">
 </head>
 <body>
 	<p><?= 'it worked baby' ?></p>
