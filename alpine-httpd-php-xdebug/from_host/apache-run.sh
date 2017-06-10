71658961679ae25e5918c71137c4a70f15db89fc
Alex Birch <Birch-san@users.noreply.github.com>
2017-06-10 17:08:38 +0100

    unnecessary double mkdir

--- alpine-httpd-php-xdebug/from_host/apache-run.sh
+++ alpine-httpd-php-xdebug/from_host/apache-run.sh
@@ -3,8 +3,6 @@
 # https://github.com/docker-library/httpd/blob/master/2.4/alpine/httpd-foreground
 set -e
 
-mkdir -p /run/apache2
-
 # Apache gets grumpy about PID files pre-existing
 rm -f /run/apache2/httpd.pid
 
