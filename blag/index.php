580b9ae91732f851dcc174e3d2ba29754569753c
Alex Birch <Birch-san@users.noreply.github.com>
2017-06-11 22:44:11 +0100

    looks like I'd been mixing preload with server push. server push is better?

--- blag/index.php
+++ blag/index.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 	<title>Hey there!</title>
-	<link rel="preload" href="style.css" as="style">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- <link rel="preload" href="style.css" as="style"> -->
 </head>
 <body>
 	<p><?= 'it worked baby' ?></p>
